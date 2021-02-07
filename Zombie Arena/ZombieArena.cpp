9497e823321057afa47a9727e0d9d8368012c058
Ryan Watson <rwatson1799@gmail.com>
2021-02-07 13:06:22 +1000

    Fix player movement

--- Zombie Arena/ZombieArena.cpp
+++ Zombie Arena/ZombieArena.cpp
@@ -83,175 +83,175 @@ int main()
 				if (state == State::PLAYING)
 				{
 				}
-			} // End event polling
+			}
+		} // End event polling
 
 			// Handle the player quitting
-			if (Keyboard::isKeyPressed(Keyboard::Escape))
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+
+		// Handle WASD while playing
+		if (state == State::PLAYING)
+		{
+			// Handle the pressing and releasing of the WASD keys
+			if (Keyboard::isKeyPressed(Keyboard::W))
 			{
-				window.close();
+				player.moveUp();
+			}
+			else
+			{
+				player.stopUp();
 			}
 
-			// Handle WASD while playing
+			if (Keyboard::isKeyPressed(Keyboard::S))
+			{
+				player.moveDown();
+			}
+			else
+			{
+				player.stopDown();
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::A))
+			{
+				player.moveLeft();
+			}
+			else
+			{
+				player.stopLeft();
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::D))
+			{
+				player.moveRight();
+			}
+			else
+			{
+				player.stopRight();
+			}
+		} // End WASD while playing
+
+		// Handle the LEVELING up state
+		if (state == State::LEVELING_UP)
+		{
+			// Handle the player LEVELING_UP
+			if (event.key.code == Keyboard::Num1)
+			{
+				state = State::PLAYING;
+			}
+
+			if (event.key.code == Keyboard::Num2)
+			{
+				state = State::PLAYING;
+			}
+
+			if (event.key.code == Keyboard::Num3)
+			{
+				state = State::PLAYING;
+			}
+
+			if (event.key.code == Keyboard::Num4)
+			{
+				state = State::PLAYING;
+			}
+
+			if (event.key.code == Keyboard::Num5)
+			{
+				state = State::PLAYING;
+			}
+
+			if (event.key.code == Keyboard::Num6)
+			{
+				state = State::PLAYING;
+			}
+
 			if (state == State::PLAYING)
 			{
-				// Handle the pressing and releasing of the WASD keys
-				if (Keyboard::isKeyPressed(Keyboard::W))
-				{
-					player.moveUp();
-				}
-				else
-				{
-					player.stopUp();
-				}
+				// Prepare the level
+				arena.width = 500;
+				arena.height = 500;
+				arena.left = 0;
+				arena.top = 0;
 
-				if (Keyboard::isKeyPressed(Keyboard::S))
-				{
-					player.moveDown();
-				}
-				else
-				{
-					player.stopDown();
-				}
+				// Pass the vertex array by reference to the createBackground function
+				int tileSize = createBackground(background, arena);
 
-				if (Keyboard::isKeyPressed(Keyboard::A))
-				{
-					player.moveLeft();
-				}
-				else
-				{
-					player.stopLeft();
-				}
+				// Spawn the player in the middle of the arena
+				player.spawn(arena, resolution, tileSize);
 
-				if (Keyboard::isKeyPressed(Keyboard::D))
-				{
-					player.moveRight();
-				}
-				else
-				{
-					player.stopRight();
-				}
-			} // End WASD while playing
-
-			// Handle the LEVELING up state
-			if (state == State::LEVELING_UP)
-			{
-				// Handle the player LEVELING_UP
-				if (event.key.code == Keyboard::Num1)
-				{
-					state = State::PLAYING;
-				}
-
-				if (event.key.code == Keyboard::Num2)
-				{
-					state = State::PLAYING;
-				}
-
-				if (event.key.code == Keyboard::Num3)
-				{
-					state = State::PLAYING;
-				}
-
-				if (event.key.code == Keyboard::Num4)
-				{
-					state = State::PLAYING;
-				}
-
-				if (event.key.code == Keyboard::Num5)
-				{
-					state = State::PLAYING;
-				}
-
-				if (event.key.code == Keyboard::Num6)
-				{
-					state = State::PLAYING;
-				}
-
-				if (state == State::PLAYING)
-				{
-					// Prepare the level
-					arena.width = 500;
-					arena.height = 500;
-					arena.left = 0;
-					arena.top = 0;
-
-					// Pass the vertex array by reference to the createBackground function
-					int tileSize = createBackground(background, arena);
-
-					// Spawn the player in the middle of the arena
-					player.spawn(arena, resolution, tileSize);
-
-					// Reset the clock so there isn't a frame jump
-					clock.restart();
-				} // End LEVELING Up
-
-				/*
-				* UPDATE THE FRAME
-				*/
-
-				if (state == State::PLAYING)
-				{
-					// Update the delta time
-					Time dt = clock.restart();
-
-					// Update the total game time
-					gameTimeTotal += dt;
-
-					// Move a decimal fraction of 1 from the delta time
-					float dtAsSeconds = dt.asSeconds();
-
-					// Where is the mouse pointer
-					mouseScreenPosition = Mouse::getPosition();
-
-					// Convert mouse position to world coordinates of mainView
-					mouseWorldPosition = window.mapPixelToCoords(Mouse::getPosition(), mainView);
-
-					// Update the player
-					player.update(dtAsSeconds, Mouse::getPosition());
-
-					// Make a note of the player's new position
-					Vector2f playerPosition(player.getCenter());
-
-					// Make the view centre around the player
-					mainView.setCenter(player.getCenter());
-				} // End updating the scene
-
-				/*
-				* DRAW THE SCENE
-				*/
-				if (state == State::PLAYING)
-				{
-					window.clear();
-
-					// set the mainView to be displayed in the window and draw everything related to it
-					window.setView(mainView);
-
-					// Draw the background
-					window.draw(background, &textureBackground);
-
-					// Draw the player
-					window.draw(player.getSprite());
-				}
-
-				if (state == State::LEVELING_UP)
-				{
-
-				}
-
-				if (state == State::PAUSED)
-				{
-
-				}
-
-				if (state == State::GAME_OVER)
-				{
-
-				}
-				
-				window.display();
+				// Reset the clock so there isn't a frame jump
+				clock.restart();
 			}
+		} // End LEVELING Up
 
-		} // End game loop
-	}
+		/*
+		* UPDATE THE FRAME
+		*/
+
+		if (state == State::PLAYING)
+		{
+			// Update the delta time
+			Time dt = clock.restart();
+
+			// Update the total game time
+			gameTimeTotal += dt;
+
+			// Move a decimal fraction of 1 from the delta time
+			float dtAsSeconds = dt.asSeconds();
+
+			// Where is the mouse pointer
+			mouseScreenPosition = Mouse::getPosition();
+
+			// Convert mouse position to world coordinates of mainView
+			mouseWorldPosition = window.mapPixelToCoords(Mouse::getPosition(), mainView);
+
+			// Update the player
+			player.update(dtAsSeconds, Mouse::getPosition());
+
+			// Make a note of the player's new position
+			Vector2f playerPosition(player.getCenter());
+
+			// Make the view centre around the player
+			mainView.setCenter(player.getCenter());
+		} // End updating the scene
+
+		/*
+		* DRAW THE SCENE
+		*/
+		if (state == State::PLAYING)
+		{
+			window.clear();
+
+			// set the mainView to be displayed in the window and draw everything related to it
+			window.setView(mainView);
+
+			// Draw the background
+			window.draw(background, &textureBackground);
+
+			// Draw the player
+			window.draw(player.getSprite());
+		}
+
+		if (state == State::LEVELING_UP)
+		{
+
+		}
+
+		if (state == State::PAUSED)
+		{
+
+		}
+
+		if (state == State::GAME_OVER)
+		{
+
+		}
+
+		window.display();
+
+	} // End game loop
 
 	return 0;
 }
